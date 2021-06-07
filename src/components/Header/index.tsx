c178c6dc2a257e6c350ca7ae5f6f4941ecbc9e40
marcoskloss <marcoskloss2001@gmail.com>
2021-06-07 18:08:19 -0300

    add Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import { ReactElement } from 'react';
+
+import styles from './header.module.scss';
+
+export default function Header(): ReactElement {
+  return (
+    <div>
+      <header className={styles.header}>
+        <Link href="/">
+          <img src="/logo.svg" alt="logo" />
+        </Link>
+      </header>
+    </div>
+  );
 }
